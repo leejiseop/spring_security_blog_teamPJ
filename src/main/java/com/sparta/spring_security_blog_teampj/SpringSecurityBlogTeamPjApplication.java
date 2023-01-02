4db00af7ee2a43de5d3f2f8ebe10fee4aa2469b8
leejiseop <lkj0245@naver.com>
2023-01-02 13:57:20 +0900

    test

---
+++ src/main/java/com/sparta/spring_security_blog_teampj/SpringSecurityBlogTeamPjApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.spring_security_blog_teampj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBlogTeamPjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityBlogTeamPjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/spring_security_blog_teampj/SpringSecurityBlogTeamPjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.spring_security_blog_teampj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityBlogTeamPjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
