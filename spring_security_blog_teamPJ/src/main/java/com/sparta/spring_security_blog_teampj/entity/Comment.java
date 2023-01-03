1958272ace11d9cdc29d4939cc47c49711b9b54c
Seongjae <cdaosldk@naver.com>
2023-01-03 20:25:04 +0900

    작업] Comment, CommentService, CommentLike, CommentController

--- spring_security_blog_teamPJ/src/main/java/com/sparta/spring_security_blog_teampj/entity/Comment.java
+++ spring_security_blog_teamPJ/src/main/java/com/sparta/spring_security_blog_teampj/entity/Comment.java
@@ -1,4 +1,35 @@
 package com.sparta.spring_security_blog_teampj.entity;
 
-public class Comment {
+import com.sparta.spring_security_blog_teampj.dto.CommentRequestDto;
+import com.sparta.spring_security_blog_teampj.dto.CommentResponseDto;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Comment extends Timestamped{
+
+    private Long id;
+
+    private Post post;
+
+    private User user;
+
+    private String username;
+
+    private String comment;
+
+    public Comment(Post post, User user, CommentRequestDto commentRequestDto) {
+        this.post = post;
+        this.user = user;
+        this.username = commentRequestDto.getUsername();
+        this.comment = commentRequestDto.getComment();
+    }
+
+    public void update(CommentRequestDto commentRequestDto) {
+        this.comment = commentRequestDto.getComment();
+    }
 }
