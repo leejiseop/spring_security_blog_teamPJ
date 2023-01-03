a9593a551b1598d13a91ca915a1b5a39887dee87
pooca <pooca12@gmail.com>
2023-01-03 16:58:01 +0900

    2차 테스트

--- spring_security_blog_teamPJ/src/main/java/com/sparta/spring_security_blog_teampj/entity/Post.java
+++ spring_security_blog_teamPJ/src/main/java/com/sparta/spring_security_blog_teampj/entity/Post.java
@@ -1,4 +1,12 @@
 package com.sparta.spring_security_blog_teampj.entity;
 
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity
+@NoArgsConstructor
 public class Post {
+
+
 }
--- spring_security_blog_teamPJ/src/main/java/com/sparta/spring_security_blog_teampj/entity/Timestamped.java
+++ spring_security_blog_teamPJ/src/main/java/com/sparta/spring_security_blog_teampj/entity/Timestamped.java
@@ -1,4 +1,6 @@
 package com.sparta.spring_security_blog_teampj.entity;
 
 public class Timestamped {
+
+
 }
