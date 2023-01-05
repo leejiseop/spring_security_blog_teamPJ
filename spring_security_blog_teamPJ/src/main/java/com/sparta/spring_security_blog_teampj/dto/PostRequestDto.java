54cba44bc78f5972d55c866b86244095c164d85d
pooca <pooca12@gmail.com>
2023-01-05 13:00:36 +0900

    작업] Post Entity, PostLike, PostRepository, PostLikeRepository, PostRequestDto, PstResponseDto

--- spring_security_blog_teamPJ/src/main/java/com/sparta/spring_security_blog_teampj/dto/PostRequestDto.java
+++ spring_security_blog_teamPJ/src/main/java/com/sparta/spring_security_blog_teampj/dto/PostRequestDto.java
@@ -1,4 +1,16 @@
 package com.sparta.spring_security_blog_teampj.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PostRequestDto {
+
+    private String title;
+    private String contents;
 }
--- spring_security_blog_teamPJ/src/main/java/com/sparta/spring_security_blog_teampj/entity/PostLike.java
+++ spring_security_blog_teamPJ/src/main/java/com/sparta/spring_security_blog_teampj/entity/PostLike.java
@@ -1,4 +1,29 @@
 package com.sparta.spring_security_blog_teampj.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@NoArgsConstructor
 public class PostLike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "POST_ID", nullable = false)
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn(name = "USER_ID", nullable = false)
+    private User user;
+
+    public PostLike(Post post, User user) {
+        this.post = post;
+        this.user = user;
+    }
 }
